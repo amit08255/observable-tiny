6ce5393f17678c89845bddd2a988dd34d4ba3340
Amit Kumar <amitcute3@gmail.com>
2022-12-10 22:07:58 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,37 +1,40 @@
-class Observable {
-  private value;
-  private subscribers = {};
+class Observable<T> {
+    private value:T;
 
-  constructor(value = null) {
-    this.value = value;
-  }
-  
-  private broadcast() {
-    Object.keys(this.subscribers).forEach(async (key) => {
-      this.subscribers[key](this.value);
-    });
-  }
-  
-  next(value) {
-    this.value = value;
-    this.broadcast();
-  }
-  
-  subscribe(func, isImmediate = false, key = 'main') {
-    this.subscribers[key] = func;
-    
-    if (isImmediate) {
-      func(this.value);
+    private subscribers:{[key:string]: (val:T) => any} = {};
+
+    constructor(value:T = null) {
+        this.value = value;
     }
-  }
-  
-  unsubscribe(key = 'main') {
-    delete this.subscribers[key];
-  }
-  
-  dispose() {
-    Object.keys(this.subscribers).forEach((key) => {
-      this.unsubscribe(key);
-    });
-  }
+
+    private broadcast() {
+        Object.keys(this.subscribers).forEach(async (key) => {
+            this.subscribers[key](this.value);
+        });
+    }
+
+    next(value:T) {
+        this.value = value;
+        this.broadcast();
+    }
+
+    subscribe(func:(val:T) => any, isImmediate = false, key = 'main') {
+        this.subscribers[key] = func;
+
+        if (isImmediate) {
+            func(this.value);
+        }
+    }
+
+    unsubscribe(key = 'main') {
+        delete this.subscribers[key];
+    }
+
+    dispose() {
+        Object.keys(this.subscribers).forEach((key) => {
+            this.unsubscribe(key);
+        });
+    }
 }
+
+export default Observable;
