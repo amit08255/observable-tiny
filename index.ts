fdb3f0cb7c85f84b00a21d718c822e9f57414359
Amit Kumar <amitcute3@gmail.com>
2022-12-10 23:57:47 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
+type SubscriberOption<T> = {
+    func:(val:T) => any,
+    isImmediate?:boolean,
+    key?:string,
+    before?:(val:T) => any,
+    after?:(val:T) => any,
+};
+
 class Observable<T> {
     private value:T;
 
     private subscribers:{[key:string]: (val:T) => any} = {};
+
+    private beforeSubscribers:{[key:string]: (val:T) => any} = {};
+
+    private afterSubscribers:{[key:string]: (val:T) => any} = {};
 
     constructor(value:T = null) {
         this.value = value;
@@ -9,25 +21,53 @@ class Observable<T> {
 
     private broadcast() {
         Object.keys(this.subscribers).forEach(async (key) => {
+            if (this.beforeSubscribers[`${key}__before__`]) {
+                this.beforeSubscribers[`${key}__before__`](this.value);
+            }
+
             this.subscribers[key](this.value);
+
+            if (this.afterSubscribers[`${key}__after__`]) {
+                this.afterSubscribers[`${key}__after__`](this.value);
+            }
         });
     }
 
     next(value:T) {
         this.value = value;
         this.broadcast();
+        return this;
     }
 
-    subscribe(func:(val:T) => any, isImmediate = false, key = 'main') {
-        this.subscribers[key] = func;
+    subscribe({
+        func, isImmediate, key, before, after,
+    }:SubscriberOption<T>) {
+        const subKey = key || 'main';
+        this.subscribers[subKey] = func;
+
+        this.beforeSubscribers[`${subKey}__before__`] = before || null;
+        this.afterSubscribers[`${subKey}__after__`] = after || null;
+
+        if (isImmediate && before) {
+            before(this.value);
+        }
 
         if (isImmediate) {
             func(this.value);
         }
+
+        if (isImmediate && after) {
+            after(this.value);
+        }
+
+        return this;
     }
 
     unsubscribe(key = 'main') {
         delete this.subscribers[key];
+        delete this.beforeSubscribers[`${key}__before__`];
+        delete this.afterSubscribers[`${key}__after__`];
+        return this;
     }
 
     dispose() {
@@ -36,5 +76,3 @@ class Observable<T> {
         });
     }
 }
-
-export default Observable;
